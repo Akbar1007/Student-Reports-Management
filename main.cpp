4461d175687ce3c13ef8c94302b64aad4d5271d6
Akbar1007 <a.akbarjon007@gmail.com>
2025-01-07 10:38:19 +0500

    Update

--- main.cpp
+++ main.cpp
@@ -3,9 +3,9 @@
 #include <cctype>
 using namespace std;
 
-void Display(int num_students, string* names, int* scores) {
-    cout << "\nStudent Data:\n";
-    for (int i = 0; i < num_students; i++) {
+void Display(int n, string* names, int* scores) {
+    cout << "\nStudent Information:\n";
+    for (int i = 0; i < n; i++) {
         if (!names[i].empty()) {
             names[i][0] = toupper(names[i][0]);
         }
@@ -13,17 +13,28 @@ void Display(int num_students, string* names, int* scores) {
     }
 }
 
-void Average(int num_students, int* scores) {
+void Average(int n, int* scores) {
     int total = 0;
     int average = 0;
-    for (int i = 0; i < num_students; i++) {
+    for (int i = 0; i < n; i++) {
         total += scores[i];
     }
-    average = total / num_students;
+    average = total / n;
 
     cout << "\nThe average score is: " << average << endl;
 }
 
+// check
+
+void TopScore(int n, int* scores) {
+    int highest_score = 0;
+    for (int i = 0; i < n; i++) {
+        if (highest_score < scores[i]) {
+            highest_score = scores[i];
+        }
+    }
+    cout << "The highest score is: " << highest_score << endl;
+}
 
 int main() {
     int num_students;
@@ -38,13 +49,18 @@ int main() {
         cin >> names[i];
         cout << "Enter score for student " << (i + 1) << ": ";
         cin >> scores[i];
-    }
+    };
 
     Display(num_students, names, scores);
     Average(num_students, scores);
+    TopScore(num_students, scores);
+    // Check_Pass(num_students, scores);
+
 
     delete[] names;
     delete[] scores;
 
     return 0;
 }
+
+
